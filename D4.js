d537c74fa8e8bf3d0fa2752741941c50bacbe4d1
Billions94 <E.a.egbedi@gmail.com>
2021-09-17 01:49:15 +0200

    Commit

--- D4.js
+++ D4.js
@@ -1,64 +1,23 @@
-// EXERCISE 1 FETCH DATA
-//    fetch("https://striveschool-api.herokuapp.com/books")
 
-//      .then(response => response.json())
-//      .then(data => console.log(data))
+let searchBooksArray = [];
+console.log(searchBooksArray)
 
-
-function searchBooks(event) {
-    const query = event.target.value;
+const query = (event) => {
+    const query = event.target.value
     console.log(query)
+    if (query.length > 2) {
+        searchBooksArray.forEach(book, index => {
+            book.remove()
+        })
+    }
+
      search()
-    
-     .then(response => response.json())
-     .then(data => {
-         console.log(data)
- 
-         const book = data
- 
-         const bookData = document.querySelector('.row')
-         bookData.innerHTML = ''
- 
-         book.forEach(book => {
-             let books = document.createElement('div')
-             books.className = 'img-fluid'
-             books.innerHTML = `       
-             <div  class="card mb-3 mr-3 " style="width: 18rem;">
-             <img src="${book.img}" class="card-img-top" alt="..."style="width: 18rem; height:400px">
-             <div class="card-body" style=" height:110px">
-             <h5 id="clamp" class="card-title">${book.title}</h5>
-             <p class="card-text m-0"></p>
-             </div>
-             <div  class="d-flex justify-content-between">
-             <a href="#" class="btn btn-warning mb-1">Add to cart</a>
-             <a href="#" class="btn btn-warning mb-1">Skip</a>
-             <div>
-           </div>`
-           bookData.appendChild(books)
-         })   
-     })
-     .catch(error => {
-       console.error(error)
-     })
-    }
-    
-    function search() {
-      return fetch("https://striveschool-api.herokuapp.com/books")      
-    }
-
-    
-
-window.onload = () => {
-    // searchBooks()
-
-    fetch("https://striveschool-api.herokuapp.com/books")
-
     .then(response => response.json())
     .then(data => {
-        console.log(data)
+        // console.log(data)
 
         const book = data
-
+        let tempArray = []
         const bookData = document.querySelector('.row')
         bookData.innerHTML = ''
 
@@ -73,11 +32,62 @@ window.onload = () => {
             <p class="card-text m-0"></p>
             </div>
             <div  class="d-flex justify-content-between">
+            <a href="#" class="btn btn-primary mb-1 ml-1"  onclick="addToCart(event, '${book.title}', '${book.img}', '€${book.price}' )" >€ ${book.price}</a>
+            <a href="#" class="btn btn-danger mb-1" onclick="skipBooks(event)" >Skip</a>
+            <div>
+          </div>`
+          bookData.appendChild(books)
+          tempArray.push(books)
+
+        }) 
+        // tempArray.filter(query => {
+        //     tempArray.includes(query)
+        //     console.log(tempArray)
+        // })  
+    })
+    .catch(error => {
+      console.error(error)
+    })
+}
+
+
+function search() {
+    return fetch("https://striveschool-api.herokuapp.com/books")      
+}
+
+    
+
+window.onload = () => {
+    // searchBooks()
+//EX 1
+    fetch("https://striveschool-api.herokuapp.com/books")
+
+    .then(response => response.json())
+    .then(data => {
+        // console.log(data)
+
+        const book = data
+
+        const bookData = document.querySelector('.row')
+        bookData.innerHTML = ''
+
+        book.forEach(book => {
+            let books = document.createElement('div')
+            books.className = 'img-fluid'
+            books.innerHTML = `       
+            <div  class="card mb-3 mr-3 " style="width: 18rem;">
+            <img src="${book.img}" class="card-img-top" alt="..."style="width: 18rem; height:400px">
+            <div class="card-body" style=" height:110px">
+            <h5 id="clamp" class="card-title">${book.title}</h5>
+            <p class="card-text m-0">${book.category}</p>
+            </div>
+            <div  class="d-flex justify-content-between">
             <a href="#" class="btn btn-info mb-1 ml-1"  onclick="addToCart(event, '${book.title}', '${book.img}', '€${book.price}' )" >€ ${book.price}</a>
             <a href="#" class="btn btn-warning mb-1" onclick="skipBooks(event)" >Skip</a>
             <div>
           </div>`
           bookData.appendChild(books)
+          searchBooksArray.push(book)
         })
         
 
@@ -102,9 +112,13 @@ const addToCart = (event, title, image, price) => {
     let books = document.createElement('a')
         books.className = 'dropdown-item'
         books.innerHTML = ` <div class="d-flex">
+        <div>
         <a href="#" id="remove" class="dropdown-item"><span><img src="${image}" alt="" width="25"></span>
         <span>${title}</span><span class="ml-2" >${price}</span></a>
+        </div>
+        <div>
         <a href="#" class="btn btn-danger mt-1 cart-btn" onclick="removeFromCart(event)">Remove</a></span></a>
+        </div>
         </div>
         `
         cart.appendChild(books)
@@ -123,10 +137,21 @@ const addToCart = (event, title, image, price) => {
         removeFromCart.remove()
  }
 
-
-const query = (event) => {
-    console.log(event.target.value)
+const bookCount = () => {
+    if(cartArrays.length > 0) {
+        // console.log(cartArrays.length)
+        let count = document.getElementById('count')
+        count.innerHTML = `Current items in your cart  (${cartArrays.length})`
+    }
 }
+
+
+
+
+
+
+
+
 
 
 
